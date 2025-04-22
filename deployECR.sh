9b849483b0afae952029c13430b991a2811cf3f3
aaddrrii <adrianasanchezaro@gmail.com>
2025-04-21 20:17:20 -0500

    commit git deplay

--- deployECR.sh
+++ deployECR.sh
@@ -1,5 +1,5 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 862695242185.dkr.ecr.us-east-1.amazonaws.com
-docker build -t products-repo-gamer-vault-ggeasy .
-docker tag products-repo-gamer-vault-ggeasy:latest 862695242185.dkr.ecr.us-east-1.amazonaws.com/products-repo-gamer-vault-ggeasy:1.0
-docker push 862695242185.dkr.ecr.us-east-1.amazonaws.com/products-repo-gamer-vault-ggeasy:1.0
+docker build -t product-services .
+docker tag product-services:latest 862695242185.dkr.ecr.us-east-1.amazonaws.com/main-repo-gamer-vault-ggeasy:product-services
+docker push 862695242185.dkr.ecr.us-east-1.amazonaws.com/main-repo-gamer-vault-ggeasy:product-services
 echo "Terminado"
